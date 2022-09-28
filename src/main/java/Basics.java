120db6d4566da514d8861c686a86d947f3af543c
ben <benx.xu@mail.utoronto.ca>
2022-09-27 22:27:22 -0400

    completed TODOs

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -44,7 +44,7 @@ public class Basics {
          *                Hello World!
          */
 
-
+        System.out.println("Hello World!");
 
         /* 4. In Python, we could declare variables using a simple assignment
          *    statement. For example:
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -98,8 +98,11 @@ public class Basics {
          *     ...
          * Current count: 1
          * Current count: 0
+         *
          */
-
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -143,6 +146,11 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] split = to_split.split(" ");
+
+        for (int i = 0; i <= split.length - 1; i++) {
+            ret.append(split[i].charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +178,13 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if (arr.length <= 1) {
+            return 0;
+        }
+
+        for (int i = 1; i <= arr.length - 1; i += 2) {
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
